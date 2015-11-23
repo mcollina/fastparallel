7f188a701c96ff74e18b029a01ee4694342d3fd5
Matteo Collina <hello@matteocollina.com>
2015-11-23 16:56:51 +0100

    Added neo-async to the benches.

--- bench.js
+++ bench.js
@@ -3,6 +3,7 @@ var parallel = require('./')()
 var parallelNoResults = require('./')({ results: false })
 var bench = require('fastbench')
 var async = require('async')
+var neo = require('neo-async')
 var insync = require('insync')
 var items = require('items')
 var parallelize = require('parallelize')
@@ -32,6 +33,10 @@ function benchInsyncParallel (done) {
   insync.parallel([somethingA, somethingA, somethingA], done)
 }
 
+function benchNeoParallel (done) {
+  neo.parallel([somethingA, somethingA, somethingA], done)
+}
+
 function benchItemsParallel (done) {
   items.parallel.execute([somethingA, somethingA, somethingA], done)
 }
@@ -48,8 +53,16 @@ function benchAsyncEach (done) {
   async.each([1, 2, 3], somethingP, done)
 }
 
+function benchNeoEach (done) {
+  neo.each([1, 2, 3], somethingP, done)
+}
+
 function benchAsyncMap (done) {
   async.map([1, 2, 3], somethingP, done)
+}
+
+function benchNeoMap (done) {
+  neo.map([1, 2, 3], somethingP, done)
 }
 
 function benchInsyncEach (done) {
@@ -95,6 +108,9 @@ var run = bench([
   benchAsyncParallel,
   benchAsyncEach,
   benchAsyncMap,
+  benchNeoParallel,
+  benchNeoEach,
+  benchNeoMap,
   benchInsyncParallel,
   benchInsyncEach,
   benchInsyncMap,
